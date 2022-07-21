097db6879aec1e2ea7410b6dd856370ee2067d2f
Hisham Paloli <hishampaloli786@gmail.com>
2022-07-21 21:12:02 +0530

    1

---
+++ pattern1.c
@@ -0,0 +1,128 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+
+    // *
+    // **
+    // ***
+    // ****
+    // *****
+    // ******
+    // *******
+
+    // int limit = 10,i,j;
+
+    // 	for(i = 1; i<= limit; i++) {
+    //         for(j = 1; j <= i; j++) {
+    // 		    printf("*");
+    //         }
+    //         printf("\n");
+    // 	}
+
+    // *****
+    // *****
+    // *****
+    // *****
+    // *****
+
+    // int limit = 5,i,j;
+
+    // for(i = 1; i <= limit; i++) {
+    //     for(j = 1; j <= limit; j++) {
+    //         printf("*");
+    //     }
+    //     printf(" \n");
+    // }
+
+    // *  *  *  *  *
+    // *  *  *  *
+    // *  *  *
+    // *  *
+    // *
+
+    // int limit = 10,i,j;
+
+    // for(i = 1; i <= limit; i++) {
+    //     for(j = 1; j <= limit - i + 1; j++) {
+    //         printf(" * ");
+    //     }
+    //     printf(" \n");
+    // }
+
+    //  1
+    //  1  2
+    //  1  2  3
+    //  1  2  3  4
+    //  1  2  3  4  5
+
+    // int count = 10,i,j;
+
+    // for ( i = 1; i <= count; i++) {
+    //     for ( j = 1; j <= i; j++){
+    //         printf(" %d ",j);
+    //     }
+    //     printf("\n");
+    // }
+
+    // *
+    // * *
+    // * * *
+    // * * * *
+    // * * * * *
+    // * * * *
+    // * * *
+    // * *
+    // *
+
+    // int count = 5,i,j;
+
+    //    for ( i = 1; i <= count * 2; i++){
+    //     int c = i <= count ? i : 2 * count - i;
+    //       for ( j = 1; j <= c; j++){
+    //         printf("* ");
+    //       }
+    //       printf("\n");
+    //    }
+
+    //     *
+    //    **
+    //   ***
+    //  ****
+    // *****
+
+    //  int count = 5,i,j,k;
+
+    //         for ( i = 1; i <= count; i++) {
+    //             for(k = 1; k <= count - i; k++){
+    //                 printf(" ");
+    //             }
+    //             for(j = 1; j <= i; j++) {
+    //             	printf("*");
+    //             }
+    //             printf("\n");
+    //         }
+
+//     *
+//    ***
+//   *****
+//  *******
+// *********
+
+    // int n = 5, m;
+    // m = n;
+    // for (int i = 1; i <= n; i++){
+    //     for (int j = 1; j <= m - 1; j++) {
+    //         printf(" ");
+    //     }
+    //      for (int k = 1; k <= 2 * i - 1; k++) {
+    //         printf("*");
+    //     }
+    //     m--;
+    //     printf("\n");
+    // }
+
+    return EXIT_SUCCESS;
+}
