4b0adbd19543086ef958884b78120428d7b9b732
Hisham Paloli <hishampaloli786@gmail.com>
2022-07-23 11:11:28 +0530

    null

---
+++ Assingments.c
@@ -0,0 +1,122 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+   // q1 :- Accept a char input from the user and display it on the console.
+
+   // code >>>>>>>>>>>>>>>>>>>>>>
+
+   // char ch1;
+   // printf("Enter a Char\n");
+   // scanf("%c", &ch1); 
+
+   // printf("You Enterd: %c", ch1);
+
+   // OUTPUT >>>>>>>>>>>>>>>>>>>
+
+   // Enter a Char
+   // 2
+   // You Enterd: 2
+
+   // *************************************************************************
+
+
+
+
+
+
+
+
+   // q2 :- Accept two inputs from the user and output its sum.
+
+
+   // code >>>>>>>>>>>>>>>>>>>>>>
+   // int number1, number2;
+   // printf("Enter a two numbers\n");
+   // scanf("%d /n", &number1); 
+   // scanf("%d", &number2); 
+
+   // int sum = number1 + number2;
+
+   // printf("Sum of the two numbers is: %d", sum);
+
+
+
+//   OUTPUT >>>>>>>>>>>>>>>>>>>
+//   Enter a two numbers
+//   0
+//   2
+//   Sum of the two numbers is: 2
+
+   // *************************************************************************
+
+
+
+
+
+
+
+   // q3 :- Write a program to find the simple interest.
+
+
+   // code >>>>>>>>>>>>>>>>>>>>>>
+   // int PrincipalAmount;
+   // float InterestRate, NumberOfYears;
+
+   // printf("Enter Principle amount, Interst rate, Number of years \n"); 
+   // scanf("%d\n", &PrincipalAmount);
+   // scanf("%f\n", &InterestRate);
+   // scanf("%f", &NumberOfYears);
+
+   // float simpleInteres = (PrincipalAmount * InterestRate * NumberOfYears) / 100;
+
+   // printf("Simple Interest is :- %f", simpleInteres);
+
+
+// OUTPUT >>>>>>>>>>>>>>>>>>>
+// Enter Principle amount, Interst rate, Number of years 
+// 10
+// 10
+// 1
+// Simple Interest is :- 1.000000
+
+   // *************************************************************************
+   
+
+
+
+
+
+
+
+   // q4 :- Write a program to find the simple interest.
+
+
+   // code >>>>>>>>>>>>>>>>>>>>>>
+   // float mark;
+
+   // printf("Enter your mark out of 100\n");
+
+   // scanf("%f", &mark);
+
+   // if (mark >= 50) {
+   //    printf("PASSED");
+   // } else (mark > -1 && mark < 50 ) {
+   //    printf("FAILED");
+   // }
+    
+   
+
+
+// OUTPUT >>>>>>>>>>>>>>>>>>>
+// Enter your mark out of 100
+// 49
+// FAILED
+
+   // *************************************************************************
+
+
+   return 0;
+}
