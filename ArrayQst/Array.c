a8dd15fd0ee689fc6054ff2b0cbb64e286e0872b
Hisham Paloli <hishampaloli786@gmail.com>
2022-07-30 14:49:28 +0530

    323

--- ArrayQst/Array.c
+++ ArrayQst/Array.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 
 int isPrime(int num) {
-  int flag = 0;
-  for (int i = 2; i <= num/2; i++)
-  {
-    if (num % i == 0)
-    {
-      flag = 1;
-      break;
-    }
+  // int flag = 0;
+  // for (int i = 2; i <= num/2; i++)
+  // {
+  //   if (num % i == 0)
+  //   {
+  //     flag = 1;
+  //     break;
+  //   }
     
-  }
+  // }
   
-  if (flag == 0)
-  {
-    return 1;
-  }else {
-    return 0;
-  }
+  // if (flag == 0)
+  // {
+  //   return 1;
+  // }else {
+  //   return 0;
+  // }
   
 }
 
