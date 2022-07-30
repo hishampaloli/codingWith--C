b87e206da38f0bed2872c9949f4790886a1b62f3
Hisham Paloli <hishampaloli786@gmail.com>
2022-07-30 14:46:36 +0530

    232wed

--- ArrayQst/Array.c
+++ ArrayQst/Array.c
@@ -24,38 +24,39 @@ int isPrime(int num) {
 
 int main() { 
   
-   int n = 8;
+   int n = 8,k = 1;
 
-   for (int i = 1; i <= n; i++)
-   {
 
-    for (int j = 1; j < i; j++)
+    for (int i = 1; i <= n; i++)
     {
-      if (i % 2 != 0)
-    {
-      if (j % 2 != 0)
+      for (int j = 1; j <= k; j++)
       {
-        printf("0");
-      }else {
-        printf("1");
+        printf("*");
       }
+
+      for (int j = 1; j <= n - i; j++)
+      {
+        printf("  ");
+      }
+
+    for (int j = 1; j <= n - i; j++)
+      {
+        printf("  ");
+      }
+
+      for (int j = 1; j <= k; j++)
+      {
+        printf("*");
+      }
+
+      k= k +2;
       
-    }else {
-       if (j % 2 != 0)
-      {
-        printf("1");
-      }else {
-        printf("0");
-      }
-    }
-    }
-    
-    
-    
 
-    printf("\n");
+      printf("\n");
+      
+    }
     
-   }
+   
    
     
    }
