015c19d233da19a3b33592e8d45399a270153332
Hisham Paloli <hishampaloli786@gmail.com>
2022-08-04 17:37:56 +0530

    w

--- ArrayQst/Array.c
+++ ArrayQst/Array.c
@@ -1,45 +1,57 @@
 #include <stdio.h>
-
-int factorial(int n)
+int isPrime(int num)
 {
-  int f;
-
-  for (f = 1; n > 1; n--)
+  int flag = 0;
+ 
+  for (int i = 2; i <= num/2 ; i++)
   {
-    f *= n;
+    if (num % i == 0)
+    {
+      flag = 1;
+      break;
+    }
+   
   }
-
-  return f;
+ 
+  if (flag == 1)
+  {
+    return 0;
+  }else {
+    return 1;
+  }
+ 
+ 
+ 
 }
-
-int icr(int n, int r)
-{
-  return factorial(n) / (factorial(n - r) * factorial(r));
-}
-
 int main()
 {
 
-  int n = 4, k = 1, i = 1;
+  int n = 5,k = 3,f = 3;
 
-  for (i = 1; i <= n ; i++)
+  for (int i = 1; i <= 5; i++)
   {
-   for (int j = 1; j <= n; j++)
-   {
-    if (j<n)
+    if (i % 2 != 0)
     {
-      printf("%d * ", k++);
-    }else {
-
-    printf("%d ", k++);
+      for (int j = 1; j <= k; j++)
+      {
+        printf("*");
+      }
+      printf("\n");
+    }else if (i % 2 == 0)
+    {
+      for (int j = 1; j <= f; j++)
+      {
+        printf("*");
+      }
+      printf("\n");
+      
     }
     
-   }
-   
     
-
-    printf("\n");
   }
+  
 
+  
   return 0;
 }
+ 
